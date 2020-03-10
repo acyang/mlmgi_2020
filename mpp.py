00c89266120e07710d7ce69568f99533c78b2d8f
acyang <acyang0903@gmail.com>
2020-03-10 11:55:41 +0800

    Add files via upload

---
+++ mpp.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 04:07:57 2020
+
+@author: 1203087
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('data.csv')
+X = dataset.iloc[:,1:2].values
+y = dataset.iloc[:,2].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Fitting the Regression Model to the dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators=100, random_state=0, )
+regressor.fit(X_train, y_train)
+
+# Visualising the Regression results for higher and smoother curve
+X_grid = np.arange(min(X_train), max(X_train), 0.01)
+X_grid = X_grid.reshape(len(X_grid), 1)
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title('Random Forest Regression')
+plt.xlabel('Density [eV]')
+plt.ylabel('Band Gap [eV]')
+plt.show()
